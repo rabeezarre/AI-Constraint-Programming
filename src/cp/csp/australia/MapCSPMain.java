528e272fee6464d0089a2a8c5705c8f229469ec2
rabeezarre <rabeezarre@gmail.com>
2024-02-01 17:27:00 +0100

    start

--- src/cp/csp/australia/MapCSPMain.java
+++ src/cp/csp/australia/MapCSPMain.java
@@ -8,23 +8,6 @@ import cp.csp.core.CSPStateListener;
 
 public class MapCSPMain {
     public static void main(String[] args) {
-        MapCSP map = new MapCSP();
-        BacktrackingStrategy bts = new BacktrackingStrategy();
-        bts.addCSPStateListener(new CSPStateListener() {
-            @Override
-            public void stateChanged(Assignment assignment, CSP csp) {
-                System.out.println("Assignment evolved : " + assignment);
-            }
-
-            @Override
-            public void stateChanged(CSP csp) {
-                System.out.println("CSP evolved : " + csp);
-            }
-        });
-        double start = System.currentTimeMillis();
-        Assignment sol = bts.solve(map);
-        double end = System.currentTimeMillis();
-        System.out.println(sol);
-        System.out.println("Time to solve = " + (end - start));
+       
     }
 }
